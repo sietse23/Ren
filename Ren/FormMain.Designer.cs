f9980c36296b6756c0bbd5a5f879f6866cec41ff
sietse23 <sietse23@gmail.com>
2014-04-29 11:30:11 +0200

    Init

---
+++ Ren/FormMain.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace Ren
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlKnoppen = new System.Windows.Forms.Panel();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnRename = new System.Windows.Forms.Button();
+            this.lbFiles = new System.Windows.Forms.ListBox();
+            this.lbRename = new System.Windows.Forms.ListBox();
+            this.ofdDialog = new System.Windows.Forms.OpenFileDialog();
+            this.fbdDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.pnlKnoppen.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlKnoppen
+            // 
+            this.pnlKnoppen.Controls.Add(this.btnClear);
+            this.pnlKnoppen.Controls.Add(this.btnOpen);
+            this.pnlKnoppen.Controls.Add(this.btnRename);
+            this.pnlKnoppen.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.pnlKnoppen.Location = new System.Drawing.Point(0, 571);
+            this.pnlKnoppen.Name = "pnlKnoppen";
+            this.pnlKnoppen.Size = new System.Drawing.Size(762, 30);
+            this.pnlKnoppen.TabIndex = 0;
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(3, 3);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(100, 23);
+            this.btnOpen.TabIndex = 1;
+            this.btnOpen.Text = "Open";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // btnRename
+            // 
+            this.btnRename.Location = new System.Drawing.Point(113, 3);
+            this.btnRename.Name = "btnRename";
+            this.btnRename.Size = new System.Drawing.Size(100, 23);
+            this.btnRename.TabIndex = 0;
+            this.btnRename.Text = "Rename";
+            this.btnRename.UseVisualStyleBackColor = true;
+            this.btnRename.Click += new System.EventHandler(this.btnRename_Click);
+            // 
+            // lbFiles
+            // 
+            this.lbFiles.AllowDrop = true;
+            this.lbFiles.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.lbFiles.FormattingEnabled = true;
+            this.lbFiles.Location = new System.Drawing.Point(3, 2);
+            this.lbFiles.Name = "lbFiles";
+            this.lbFiles.Size = new System.Drawing.Size(371, 563);
+            this.lbFiles.TabIndex = 1;
+            this.lbFiles.DragDrop += new System.Windows.Forms.DragEventHandler(this.lbFiles_DragDrop);
+            this.lbFiles.DragOver += new System.Windows.Forms.DragEventHandler(this.lbFiles_DragOver);
+            this.lbFiles.KeyDown += new System.Windows.Forms.KeyEventHandler(this.lbFiles_KeyDown);
+            this.lbFiles.MouseDown += new System.Windows.Forms.MouseEventHandler(this.lbFiles_MouseDown);
+            // 
+            // lbRename
+            // 
+            this.lbRename.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbRename.FormattingEnabled = true;
+            this.lbRename.Location = new System.Drawing.Point(388, 2);
+            this.lbRename.Name = "lbRename";
+            this.lbRename.Size = new System.Drawing.Size(371, 563);
+            this.lbRename.TabIndex = 2;
+            this.lbRename.KeyDown += new System.Windows.Forms.KeyEventHandler(this.lbRename_KeyDown);
+            // 
+            // fbdDialog
+            // 
+            this.fbdDialog.ShowNewFolderButton = false;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnClear.Location = new System.Drawing.Point(659, 4);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(100, 23);
+            this.btnClear.TabIndex = 2;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(762, 601);
+            this.Controls.Add(this.lbRename);
+            this.Controls.Add(this.lbFiles);
+            this.Controls.Add(this.pnlKnoppen);
+            this.Name = "FormMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Rename";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FormMain_FormClosing);
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            this.pnlKnoppen.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlKnoppen;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Button btnRename;
+        private System.Windows.Forms.ListBox lbFiles;
+        private System.Windows.Forms.ListBox lbRename;
+        private System.Windows.Forms.OpenFileDialog ofdDialog;
+        private System.Windows.Forms.FolderBrowserDialog fbdDialog;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
---
+++ Ren/FormMain.cs
@@ -0,0 +1,326 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Ren.Properties;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace Ren
+{
+    public partial class FormMain : Form
+    {
+        #region Variables
+        /********************************************** Variables ******************************************************************/
+
+        private Settings m_Settings;
+
+        private string m_LastDir = @"D:\Film\Series";
+        private string m_FileDir = "";
+
+        private string m_Extenstion = "";
+
+        #endregion Variables
+
+        #region Constructor, Load & Closing
+        /********************************************** Constructor, Load & Closing ************************************************/
+
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            Init();
+
+            this.Top = 100;
+            this.Height = Screen.PrimaryScreen.WorkingArea.Height - 200;
+            this.Left = 900;
+
+            AddClipboardFormatListener(this.Handle);
+        }
+
+        private void FormMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            RemoveClipboardFormatListener(this.Handle);
+        }
+
+        #endregion Constructor, Load & Closing
+
+        #region Button Events
+        /********************************************** Button Events **************************************************************/
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            OpenFiles();
+        }
+
+        private void btnRename_Click(object sender, EventArgs e)
+        {
+            Rename();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            lbRename.Items.Clear();
+        }
+
+        #endregion Button Events
+
+        #region Control Events
+        /********************************************** Control Events *************************************************************/
+
+        private void lbFiles_MouseDown(object sender, MouseEventArgs e)
+        {
+            int myIndex = lbFiles.IndexFromPoint(e.Location);
+
+            if (myIndex > -1)
+            {
+                lbFiles.DoDragDrop(lbFiles.Items[myIndex], DragDropEffects.Move);
+            }
+        }
+
+        private void lbFiles_DragDrop(object sender, DragEventArgs e)
+        {
+            Point myPoint = lbFiles.PointToClient(new Point(e.X, e.Y));
+
+            int myIndex = lbFiles.IndexFromPoint(myPoint);
+            if (myIndex < 0) myIndex = lbFiles.Items.Count - 1;
+
+            object myData = e.Data.GetData(typeof(string));
+            lbFiles.Items.Remove(myData);
+            lbFiles.Items.Insert(myIndex, myData);            
+        }
+
+        private void lbFiles_DragOver(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.Move;
+        }
+
+        private void lbFiles_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (lbFiles.SelectedItem != null)
+            {
+                lbFiles.Items.Remove(lbFiles.SelectedItem);
+            }
+        }
+
+        private void lbRename_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (lbRename.SelectedItem != null)
+            {
+                lbRename.Items.Remove(lbRename.SelectedItem);
+            }
+        }
+
+        #endregion Control Events
+
+        #region Functions
+        /********************************************** Functions ******************************************************************/
+
+        private void Init()
+        {
+            m_Settings = new Settings();
+            m_Settings.Reload();
+
+            if (!string.IsNullOrEmpty(m_Settings.LastDir))
+            {
+                if (Directory.Exists(m_Settings.LastDir))
+                {
+                    m_LastDir = m_Settings.LastDir;
+                }
+                else
+                {
+                    string myDir = Path.GetDirectoryName(m_Settings.LastDir);
+
+                    while (myDir != null && !Directory.Exists(myDir))
+                    {
+                        myDir = Path.GetDirectoryName(myDir);
+                    }
+
+                    if (Directory.Exists(myDir))
+                    {
+                        m_LastDir = m_Settings.LastDir = myDir;
+                        m_Settings.Save();
+                    }
+                }
+            }
+        }
+
+        private void OpenFiles()
+        {
+            lbFiles.Items.Clear();
+            lbRename.Items.Clear();
+
+            fbdDialog.SelectedPath = m_LastDir;
+            fbdDialog.RootFolder = Environment.SpecialFolder.MyComputer;
+
+            using (new CenterWinDialog(this))
+            {
+
+                if (fbdDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    m_LastDir = m_Settings.LastDir = fbdDialog.SelectedPath;
+                    m_Settings.Save();
+
+                    DirectoryInfo myDirInfo = new DirectoryInfo(fbdDialog.SelectedPath);
+
+                    if (myDirInfo.GetFiles("*", SearchOption.AllDirectories).Length > 0)
+                    {
+                        foreach (FileInfo myFileInfo in myDirInfo.GetFiles("*", SearchOption.AllDirectories))
+                        {
+                            m_Extenstion = myFileInfo.Extension;
+                            m_FileDir = Path.GetDirectoryName(myFileInfo.FullName);
+
+                            AddFile(myFileInfo.FullName);
+
+                        }
+                    }
+                }
+            }
+        }
+
+        private void AddFile(string FileName)
+        {
+            lbFiles.Items.Add(Path.GetFileNameWithoutExtension(FileName));
+        }
+
+        private void RenameFile(string OldName, string NewName)
+        {
+            try
+            {
+                OldName = string.Format("{0}{1}", Path.Combine(m_FileDir, OldName), m_Extenstion);
+                NewName = string.Format("{0}{1}", Path.Combine(m_FileDir, NewName), m_Extenstion);
+
+                File.Move(OldName, NewName);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Rename()
+        {
+            if (lbFiles.Items.Count == lbRename.Items.Count)
+            {
+                for (int i = 0; i < lbFiles.Items.Count; i++)
+                {
+                    string myOld = lbFiles.Items[i].ToString();
+                    string myNew = lbRename.Items[i].ToString();
+
+                    RenameFile(myOld, myNew);
+                }
+
+                lbFiles.Items.Clear();
+                lbRename.Items.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Ongelijk aantal");
+            }
+        }
+
+        private void AddRegel(string Tekst)
+        {
+            Tekst = Tekst.Replace(@"\", "").Replace(@"/", "").Replace(@"*", "").Replace(@"?", "").Replace("\"", "").Replace(@"<", "").Replace(@">", "").Replace(@"|", "").Replace(@":", "");
+
+            lbRename.Items.Add(Tekst);
+        }
+
+        private void VerwerkRegel(string Tekst)
+        {
+            if (!string.IsNullOrEmpty(Tekst))
+            {
+                try
+                {
+                    string myNummer = Tekst.Substring(Tekst.IndexOf("-") + 1, 2);
+
+                    string myTitel = Tekst.Substring(Tekst.IndexOf("-") + 3, Tekst.Length - (Tekst.IndexOf("-") + 3));
+
+                    myTitel = Tekst.Substring(Tekst.IndexOf("/") + 8, Tekst.Length - (Tekst.IndexOf("/") + 8));
+
+                    myTitel = myTitel.Replace("[Recap]", "").Replace("[Trailer]", "");
+
+
+                    AddRegel(string.Format("{0} {1}", myNummer, myTitel.Trim()));
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        private void VerwerkClipboard(string Tekst)
+        {
+            if (lbFiles.Items.Count > 0)
+            {
+                string[] myRegels = Tekst.Split(Environment.NewLine.ToCharArray());
+
+                foreach (string myRegel in myRegels)
+                {
+                    VerwerkRegel(myRegel.Trim());
+                }
+            }
+        }
+	
+        protected override void WndProc(ref Message m)
+        {
+            base.WndProc(ref m);
+ 
+            if (m.Msg == WM_CLIPBOARDUPDATE)
+            {
+                IDataObject iData = Clipboard.GetDataObject();     
+
+                if (iData.GetDataPresent(DataFormats.Text))
+                {
+                    string text = (string)iData.GetData(DataFormats.Text);
+                    VerwerkClipboard(text);
+                }
+            }
+        }
+
+        #endregion Functions
+
+        #region Properties
+        /********************************************** Properties *****************************************************************/
+
+        #endregion Properties
+
+        #region External
+        /********************************************** External *****************************************************************/
+
+        /// <summary>
+        /// Places the given window in the system-maintained clipboard format listener list.
+        /// </summary>
+        [DllImport("user32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool AddClipboardFormatListener(IntPtr hwnd);
+
+        /// <summary>
+        /// Removes the given window from the system-maintained clipboard format listener list.
+        /// </summary>
+        [DllImport("user32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool RemoveClipboardFormatListener(IntPtr hwnd);
+
+        /// <summary>
+        /// Sent when the contents of the clipboard have changed.
+        /// </summary>
+        private const int WM_CLIPBOARDUPDATE = 0x031D;
+
+        #endregion External
+
+
+
+
+
+    }
+}
